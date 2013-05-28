b456459a2eb0eb2ed5975bcaa35e99f2da6edbe7
arthinking <arthinking@itzhai.com>
2013-05-29 01:02:10 +0800

    add files

---
+++ pv-show/background.js
@@ -0,0 +1,28 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(null, {code: "goTop(" + string2Bool(localStorage.animation) + ");", allFrames: true});
+});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	initialize(tabId);
+});
+chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
+	initialize(tabId);
+});
+chrome.extension.onRequest.addListener(
+  function() {
+    chrome.tabs.executeScript(null, {code: "goTop(" + string2Bool(localStorage.animation) + ");", allFrames: true});
+});
+
+function initialize(tabId){
+	chrome.tabs.executeScript(tabId, {file: "top.js"});
+	chrome.tabs.insertCSS(tabId, {file: "style.css"});
+	chrome.tabs.executeScript(tabId, {file: "frame.js", allFrames: true}, function(){
+		chrome.tabs.executeScript(tabId,
+							{code: "initIcon(" + string2Bool(localStorage.addIcon) + ");"});
+		chrome.tabs.executeScript(tabId,
+							{code: "initHotkeys(" + string2Bool(localStorage.hotkeys) + ");", allFrames: true});
+	});
+}
+
+function string2Bool(string){
+	return string=='false'?false:true;
+}
