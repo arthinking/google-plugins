89d93b06add737285b02bb9ecc153632b9c0bde4
arthinking <arthinking@itzhai.com>
2013-06-06 00:56:08 +0800

    add change

--- example/browser-action/popup.js
+++ example/browser-action/popup.js
@@ -12,14 +12,15 @@ document.addEventListener('DOMContentLoaded', function(){
 	var obj = {"name":"root", "password":"123"};
 	for(var i=0; i<divs.length; i++){
 		divs[i].addEventListener('click', function(e){
-			// var jsonText = JSON.stringify(e);
+			// var jsonText = JSON.stringify(e);  // 转换报错
 			// console.log(jsonText);
 			// chrome.tabs.executeScript(null,
-			// 	{code:"switchLight('"+ obj +"');", allFrames: true}); // 这里如果传递一个e事件对象的话，会自动转换为字符串，导致对象变成了字符串
-			// chrome.tabs.executeScript(null,
-			// 	{code:"switchLight('"+ e.target.id +"');", allFrames: true}); // 这里如果传递一个e事件对象的话，由于跨页面了，导致对象变成了字符串
+			// 	{code:"switchLight('"+ obj +"');", allFrames: true}); // 这里如果传递一个e事件对象的话，会自动转换为字符串字面值，导致对象变成了字符串
+			// 向页面注入JavaScript 脚本执行，由于这里调用的是一个JS方法switchLight()，该方法在func.js文件中，所以可以在background.js中把该JS（content script）注入到web页面中。
+			chrome.tabs.executeScript(null,
+			 	{code:"switchLight('"+ e.target.id +"');", allFrames: true});
 			// console.log("send");
-			chrome.extension.sendRequest(e);   // 可以传递JSON对象
+			// chrome.extension.sendRequest(e);   // 发送请求方法可以传递JSON对象，但是这里chrome把e事件对象转换为JSON字符串时会报错：Uncaught TypeError: Converting circular structure to JSON
 		});		
 	}
 });
