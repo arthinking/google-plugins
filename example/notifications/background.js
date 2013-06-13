1ef010fe61a0c933dc33657a441d81c962075b6c
arthinking <arthinking@itzhai.com>
2013-06-13 23:52:04 +0800

    add changes

--- example/notifications/background.js
+++ example/notifications/background.js
@@ -1,49 +1,54 @@
-/*
-  Displays a notification with the current time. Requires "notifications"
-  permission in the manifest file (or calling
-  "webkitNotifications.requestPermission" beforehand).
-*/
 /**
- * 显示一个时间 notification 
+ * 显示一个时间 notification
  */
 function show() {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
-  // 创建一个notification
-  // http://www.cnblogs.com/hupeng/archive/2012/10/05/2712326.html
-  var notification = window.webkitNotifications.createNotification(
-    '48.png',                      // The image.
-    hour + time[2] + ' ' + period, // The title.
-    'Time to make the toast.'      // The body.
-  );
-  // 显示notification
-  notification.show();
+	var time = new Date().format('yyyy-MM-dd hh:mm:ss');
+  	// 创建一个notification
+ 	var notification = window.webkitNotifications.createNotification(
+    	'48.png',	// 图片，在web_accessible_resources 中添加了
+    	'现在的时间是：',	// title
+    	time	// body.
+  	);
+  	// 显示notification
+  	notification.show();
 }
 
-// Conditionally initialize the options.
-if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;   // The display activation.
-  localStorage.frequency = 1;        // The display frequency, in minutes.
-  localStorage.isInitialized = true; // The option initialization.
-}
-
-// Test for notification support.
-if (window.webkitNotifications) {
-  // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
-
-  var interval = 0; // The display interval, in minutes.
-
-  setInterval(function() {
-    interval++;
-
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
-      interval = 0;
+// 格式化时间函数
+Date.prototype.format = function(format){
+    var o = {
+    "M+" : this.getMonth()+1, //month
+    "d+" : this.getDate(),    //day
+    "h+" : this.getHours(),   //hour
+    "m+" : this.getMinutes(), //minute
+    "s+" : this.getSeconds(), //second
+    "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+    "S" : this.getMilliseconds() //millisecond
     }
-  }, 60000);
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+    (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)if(new RegExp("("+ k +")").test(format))
+    format = format.replace(RegExp.$1,
+    RegExp.$1.length==1 ? o[k] :
+    ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
+}
+
+// 测试浏览器是否支持 webkitNotifications
+if(window.webkitNotifications) {
+	// 显示通知
+  	show();
+  	var interval = 0;
+	// 弹出10次
+	var times = 10;
+	// 创建定时器
+  	var timer = setInterval(function() {
+    	interval++;
+		// 10秒钟弹出一次
+    	if (10 <= interval) {
+      		show();
+      		interval = 0;
+      		times--;
+      		if(times <- 0) clearInterval(timer);
+    	}
+  	}, 1000);
 }
